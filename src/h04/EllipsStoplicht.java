6cabf212c548d2741a56e0bc6e81b5d5f1a323d6
JamesWagimin <2140431@talnet.nl>
2020-09-09 10:02:46 +0200

    start ellips

---
+++ src/h04/EllipsStoplicht.java
@@ -0,0 +1,16 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class EllipsStoplicht extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.fillArc(10,10,40,20,180,90);
+
+    }
+}
