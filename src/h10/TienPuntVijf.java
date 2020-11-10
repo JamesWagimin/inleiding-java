111007ed10fd2e73eb98aa4584f2f0435fb1a132
JamesWagimin <2140431@talnet.nl>
2020-11-10 11:55:25 +0100

    10.5 af

--- src/h10/TienPuntVijf.java
+++ src/h10/TienPuntVijf.java
@@ -10,8 +10,9 @@ public class TienPuntVijf extends Applet {
     TextField vak;
     Label label;
     Button button;
-    double input, gemiddelde, tussen, tussen2;
-    String s, t;
+    double input, gemiddelde, tussen;
+    String s, t, welniet;
+    int intgemiddeld;
 
     public void init() {
         vak = new TextField("", 5);
@@ -23,6 +24,9 @@ public class TienPuntVijf extends Applet {
         add(vak);
         add(button);
 
+        gemiddelde = 0.0;
+        tussen = 0;
+
 
     }
 
@@ -30,8 +34,8 @@ public class TienPuntVijf extends Applet {
     public void paint(Graphics g) {
 
 g.drawString(t, 10 ,100);
-g.drawString("je gemiddelde is " + tussen,10,200);
-
+g.drawString("Je gemiddelde is " + gemiddelde,10,200);
+g.drawString("Je bent " + welniet + " geslaagd",10,220);
     }
 
     class Buttonlistener implements ActionListener{
@@ -39,9 +43,14 @@ g.drawString("je gemiddelde is " + tussen,10,200);
           s= vak.getText();
           input = Double.parseDouble( s);
 
-
-//          tussen = Double.parseDouble(s);
-//          gemiddelde = (cijfer + tussen) / 2;
+            gemiddelde = ((gemiddelde * tussen) + input) / (tussen + 1);
+            gemiddelde =  gemiddelde * 10;
+            intgemiddeld = (int) gemiddelde;
+            gemiddelde =  intgemiddeld;
+            gemiddelde = gemiddelde / 10;
+            tussen++;
+            if (gemiddelde >= 5.5){ welniet = "wel";
+            } else welniet = "niet";
 
 
           if (input >= 5.5 ) { t= "Je hebt een voldoende";
